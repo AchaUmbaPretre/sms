8eb7c1a57254528eefdcc46910e8576637241257
AchaUmbaPretre <[achandambi@gmail.com]>
2026-02-21 21:43:13 +0100

    commit

--- src/shared/api/endpoints.js
+++ src/shared/api/endpoints.js
@@ -1,26 +1,5 @@
-import { api } from "./apiClient";
-
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      console.warn("Session expirée");
-      // logout automatique possible ici
-    }
-
-    return Promise.reject(error);
-  }
-);
+export const ENDPOINTS = {
+  MATIERES: "/matieres",
+  ELEVES: "/eleves",
+  PROFESSEURS: "/professeurs",
+};
